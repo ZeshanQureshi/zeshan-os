ac6c72fecfa4008adc010be0aef2b4107f437a11
ZeshanQureshi <z22qures@gmail.com>
2022-07-13 18:58:56 -0400

    Removed Animation Dependencies

--- src/main.js
+++ src/main.js
@@ -3,14 +3,9 @@ import App from './App.vue'
 
 import vClickOutside from "click-outside-vue3";
 
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-
 const app = createApp(App);
 
 app.use(vClickOutside);
 
-
-AOS.init();
 
 app.mount('#app');
